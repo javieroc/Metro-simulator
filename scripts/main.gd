a5c890141d87a08d32910c40b50a11f9458c021c
Javier Ocampo <ferroxido@gmail.com>
2026-02-20 18:13:59 +0000

    feat: light blue metro line

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,24 +1,35 @@
 extends Node2D
 
 const Train = preload("res://scripts/train.gd")
+
 @onready var info_panel = $CanvasLayer/UIPanel
 
 func _ready():
 	connect_all_station_signals()
-
-	for line_node in $Tracks.get_children():
-		var line := line_node as MetroLine
-		if not line:
-			continue
-
+	for line in get_all_metro_lines():
 		if not line.train_spawned.is_connected(_on_train_spawned):
 			line.train_spawned.connect(_on_train_spawned)
 
+func get_all_metro_lines() -> Array:
+	var lines := []
+	_collect_metro_lines($Tracks, lines)
+	return lines
+
+func _collect_metro_lines(node: Node, result: Array):
+	for child in node.get_children():
+		if child is MetroLine:
+			result.append(child)
+		else:
+			_collect_metro_lines(child, result)
+
 func connect_all_station_signals():
-	for line in $Tracks.get_children():
-		var stations_node = line.get_node("Stations")
+	for line in get_all_metro_lines():
+		var stations_node = line.get_node_or_null("Stations")
+		if not stations_node:
+			continue
 		for station in stations_node.get_children():
-			station.station_clicked.connect(_on_station_clicked)
+			if not station.station_clicked.is_connected(_on_station_clicked):
+				station.station_clicked.connect(_on_station_clicked)
 
 func _on_train_spawned(train):
 	if not train.train_clicked.is_connected(_on_train_clicked):
